c70a8c4533778b55225a0b87c14e7f5a56a18ecf
Raphael Creer <rcreer@lab3r4s4.42abudhabi.ae>
2024-10-09 16:42:44 +0400

    added multiple fd

--- ver6_bonus/pipex_bonus.c
+++ ver6_bonus/pipex_bonus.c
@@ -13,20 +13,19 @@ void wait_for_processs(int ac)
 }
 int main(int ac,char **av, char **envp)
 {   
-    if(ac == 5)
+    if(ac >= 5)
     {   
         t_pipex pipex;
         int end[2];
         int pid_two;
-        int pid_one;
         int status;
 
         if(pipe(end) == -1)
             error_msg();
-        pid_one = process_one(end,av,envp);
+        process_one(end,av,envp);
+        multiple_pipes(ac,end,av,envp);
         pid_two = process_two(ac,end,av,envp);
         closing(end,-1);
-        waitpid(pid_one,&status,0);
         waitpid(pid_two,&status,0);
         wait_for_processs(ac);
         if(WIFEXITED(status))
@@ -36,6 +35,6 @@ int main(int ac,char **av, char **envp)
         exit(EXIT_SUCCESS);
     }
     else
-        putstr_fd("\033[31mEnter Valid Input: \033[0m<file1> <cmd1> <cmd2> <file2>\n",1);
+        putstr_fd("\033[31mEnter Valid Input: \033[0m<file1> <cmd1> <cmd2> ..... <file2>\n",1);
     return(0);
 }
